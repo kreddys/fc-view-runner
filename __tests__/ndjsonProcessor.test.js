698f37bf9ad7f9c03891e29127a4dfe640b5f37f
kreddy <kishore5214@outlook.com>
2025-03-15 18:14:04 -0500

    tested extensions in fhirpath

--- __tests__/ndjsonProcessor.test.js
+++ __tests__/ndjsonProcessor.test.js
@@ -19,6 +19,7 @@ describe('NdjsonProcessor', () => {
   const invalidDataPath = path.join(__dirname, 'fixtures', 'invalid.ndjson');
   const largeDataPath = path.join(__dirname, 'fixtures', 'large.ndjson');
   const referenceDataPath = path.join(__dirname, 'fixtures', 'reference.ndjson');
+  const extensionDataPath = path.join(__dirname, 'fixtures', 'extension.ndjson');
 
   beforeAll(() => {
     // Create test NDJSON files
@@ -40,11 +41,83 @@ describe('NdjsonProcessor', () => {
       JSON.stringify({ resourceType: 'Observation', id: '3', status: 'final', subject: {}, valueQuantity: { value: 98.6 } }) // Missing reference
     ].join('\n');
 
+    // Test data for extensions
+    const extensionData = [
+      JSON.stringify({
+        resourceType: 'Patient',
+        id: '1238',
+        meta: { /* ... */ },
+        text: { /* ... */ },
+        extension: [
+          {
+            url: 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-race',
+            extension: [
+              {
+                url: 'ombCategory',
+                valueCoding: {
+                  system: 'urn:oid:2.16.840.1.113883.6.238',
+                  code: '2106-3',
+                  display: 'White'
+                }
+              },
+              {
+                url: 'text',
+                valueString: 'White'
+              }
+            ]
+          },
+          {
+            url: 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-ethnicity',
+            extension: [
+              {
+                url: 'ombCategory',
+                valueCoding: {
+                  system: 'urn:oid:2.16.840.1.113883.6.238',
+                  code: '2186-5',
+                  display: 'Not Hispanic or Latino'
+                }
+              },
+              {
+                url: 'text',
+                valueString: 'Not Hispanic or Latino'
+              }
+            ]
+          },
+          {
+            url: 'http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName',
+            valueString: 'Freida957 O\'Reilly797'
+          },
+          {
+            url: 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-birthsex',
+            valueCode: 'M'
+          },
+          {
+            url: 'http://hl7.org/fhir/StructureDefinition/patient-birthPlace',
+            valueAddress: {
+              city: 'Memphis',
+              state: 'Tennessee',
+              country: 'US'
+            }
+          }
+        ],
+        identifier: [ /* ... */],
+        name: [ /* ... */],
+        telecom: [ /* ... */],
+        gender: 'male',
+        birthDate: '1970-07-09',
+        address: [ /* ... */],
+        maritalStatus: { /* ... */ },
+        multipleBirthBoolean: false,
+        communication: [ /* ... */]
+      })
+    ].join('\n');
+
     fs.writeFileSync(testDataPath, testData);
     fs.writeFileSync(emptyDataPath, '');
     fs.writeFileSync(invalidDataPath, 'invalid json\n' + testData);
     fs.writeFileSync(largeDataPath, largeData);
     fs.writeFileSync(referenceDataPath, referenceData);
+    fs.writeFileSync(extensionDataPath, extensionData);
   });
 
   afterAll(() => {
@@ -54,6 +127,7 @@ describe('NdjsonProcessor', () => {
     fs.unlinkSync(invalidDataPath);
     fs.unlinkSync(largeDataPath);
     fs.unlinkSync(referenceDataPath);
+    fs.unlinkSync(extensionDataPath);
   });
 
   it('should process NDJSON file with basic columns', async () => {
@@ -176,6 +250,35 @@ describe('NdjsonProcessor', () => {
       observation_id: '3',
       patient_id: null, // Missing reference should return null
       value: 98.6
+    });
+  });
+
+  it('should extract extension data into views', async () => {
+    const options = {
+      columns: [
+        { path: 'id', name: 'patient_id' },
+        { path: 'gender', name: 'gender' },
+        { path: "extension.where(url = 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-race').extension.where(url = 'text').valueString", name: 'race' },
+        { path: "extension.where(url = 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-ethnicity').extension.where(url = 'ombCategory').valueCoding.display", name: 'ethnicity' },
+        { path: "extension.where(url = 'http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName').valueString", name: 'mothers_maiden_name' },
+        { path: "extension.where(url = 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-birthsex').valueCode", name: 'birth_sex' },
+        { path: "extension.where(url = 'http://hl7.org/fhir/StructureDefinition/patient-birthPlace').valueAddress.city", name: 'birth_city' }
+      ],
+      resource: 'Patient'
+    };
+
+    const results = await processNdjson(extensionDataPath, options);
+    expect(results).toHaveLength(1);
+
+    // Check the extracted extension data
+    expect(results[0]).toEqual({
+      patient_id: '1238',
+      gender: 'male',
+      race: 'White',
+      ethnicity: 'Not Hispanic or Latino',
+      mothers_maiden_name: 'Freida957 O\'Reilly797',
+      birth_sex: 'M',
+      birth_city: 'Memphis'
     });
   });
 });
